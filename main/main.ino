31696197ab654c6fecbe392d2b022d6d9e5d1555
Jason2866 <24528715+Jason2866@users.noreply.github.com>
2020-10-16 11:02:21 +0200

    Core 2.3.0

--- main/main.ino
+++ main/main.ino
@@ -9,23 +9,23 @@
 #include <ESP8266HTTPUpdateServer.h>
 
 char apSSID[] = "UpdateMe";
-char apPass[] = "updater1";
+char apPass[] = "recovery";
 
 const char PAGE_index[] PROGMEM = R"=====(
 <!DOCTYPE html>
 <html><head><title>Flash this ESP8266!</title></head><body>
 <h2>Welcome!</h2>
-You are successfully connected to your ESP8266 via its WiFi.<br>
+You are successfully connected to your ESP8266.<br>
 Please click the button to proceed and upload a new binary firmware!<br><br>
-<b>Be sure to double check the firmware (.bin) is suitable for your chip!<br>
-I am not to be held liable if you accidentally flash a cat pic instead or something goes wrong during the update!<br>
+<b>Be sure to double check the firmware (.bin) is suitable!<br>
+I am not to be held liable if you accidentally flash a wrong file or something goes wrong during the update!<br>
 You are solely responsible for using this tool!</b><br><br>
 <form><input type="button" value="Select firmware..." onclick="window.location.href='/update'" />
 </form><br>
-(c) 2017 Christian Schwinne <br>
+(c) 2020 Christian Schwinne and Johann Obermeier<br>
 <i>Licensed under the MIT license</i> <br>
 <i>Uses libraries:</i> <br>
-<i>ESP8266 Arduino Core</i> <br>
+<i>ESP8266 Arduino Core 2.3.0</i> <br>
 </body></html>
 )=====";
 
